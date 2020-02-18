ff229bbf571c73905a08f11ca1bdba5093506694
9pepe7 <pepenc97@hotmail.com>
2020-02-18 17:23:26 +0100

    Comienzo a hacer las dos personas ficticias

--- P1/Personas/UX-DIU-Toolkit-master/Persona.js
+++ P1/Personas/UX-DIU-Toolkit-master/Persona.js
@@ -1,10 +1,10 @@
 /*******************************************/
 /*             PERSONA.JS                  */
-/*     Datos para PERSONA TEMPLATE         */   
-/*          [DIU] UX Toolkit               */                        
+/*     Datos para PERSONA TEMPLATE         */
+/*          [DIU] UX Toolkit               */
 /*          ver 1.0, 2019                  */
 /*******************************************/
-    
+
 /****  README:       */
 /****  Modifica los datos para las Personas      */
 /****  Las imagenes para  'Photo'  están en carpeta ./photos **/
@@ -15,25 +15,25 @@
 
 
 angular.module("angular", [])
-	.controller("controller", ["$scope", function($scope) { 
-		
+	.controller("controller", ["$scope", function($scope) {
+
 		$scope.PersonaIndex = 0;
 		$scope.Personas = [
-			{		
-                
+			{
+
                 /*************************************/
                 /**** PRIMERA PERSONA          *******/
                 /*** Cambiar datos             *******/
                 /*************************************/
-                
-                
+
+
 				Id: 0,
-				Name: "Juan Lopez",
+				Name: "Travis Bickle",
 				Photo: "man.png",
-				Quote: "Carpe Diem",
+				Quote: "Arriba España",
 				Age: 32,
 				Occupation: "Comercial en empresa de automocion",
-				Family: "Con pareja desde hace 1 año",
+				Family: "Abascal es mi padre",
 				Location: "Granada (Huetor Vega)",
 				Character: "Le gusta disfrutar del tiempo",
 				PersonalityTraits: [
@@ -41,7 +41,7 @@ angular.module("angular", [])
 					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
-				], 
+				],
 				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
 				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
 				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
@@ -50,9 +50,9 @@ angular.module("angular", [])
 					{ Name: "Movil", Value: 2 },
 					{ Name: "RRSS", Value: 3 },
 					{ Name: "Software", Value: 2 }
-					
-				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+
+				],
+                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional", Value: 5 },
 					{ Name: "Online & Social Media", Value: 3 },
@@ -60,39 +60,39 @@ angular.module("angular", [])
 					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
 				]
 			},
-			{	
-                
+			{
+
                 /*************************************/
                 /**** SEGUNDA PERSONA          *******/
                 /*** Cambiar datos             *******/
                 /*************************************/
-                
-                
+
+
 				Id: 1,
-				Name: "Monica Suarez",
-				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Name: "Ellen Ripley",
+				Photo: "Persona2.jpg",
+				Quote: "Tempus fugit",
+				Age: 23,
+				Occupation: "Enfermera",
+				Family: "Padres divorciados, una hermana y un sobrino",
+				Location: "La Laguna (Tenerife)",
+				Character: "Decidida, valiente y algo alocada",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
-				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
+					{ Name: "Introvertida/reservada Vs  Extrov/activa ", Value: 5 },
+					{ Name: "Realista/práctica  Vs    Intuición/imaginativa", Value: 3 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsiva", Value: 4 },
+					{ Name: "Flemática/apática  Vs   Colérica/visceral", Value: 2 }
+				],
+				Goals: ["Desarrollarse como enfermera", "Afianzar sus amistades", "Vivir el máximo de experiencias posibles"],
+				Frustrations: ["Sentirse con nada que hacer", "No conocer el sitio donde vive", "The problems with the solutions already available.", "The product or service which does not currently exist."],
 				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
+					{ Name: "TIC/Internet", Value: 2 },
 					{ Name: "Mobile", Value: 3 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
-					
-				], 
+					{ Name: "RRSS", Value: 4 },
+					{ Name: "Software", Value: 1 }
+
+				],
                 Contextos:   "The goals this user hopes to achieve." ,
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
